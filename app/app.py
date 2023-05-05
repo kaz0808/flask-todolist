27937e52b543a325663d32458a9a7a67283d36bb
kaz0808 <kaz@realidea.jp>
2023-05-05 15:37:04 +0900

    もとに戻す機能追加

--- app/app.py
+++ app/app.py
@@ -16,9 +16,9 @@ def get_db_connection():
 def main_page():
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM todo_list')
+    cursor.execute('SELECT * FROM tasks where completed = 0')
     todos = cursor.fetchall()
-    cursor.execute('SELECT * FROM complete_list')
+    cursor.execute('SELECT * FROM tasks where completed = 1')
     completes = cursor.fetchall()
     conn.close()
     return render_template('index.html', todos=todos,completes=completes)
@@ -28,7 +28,7 @@ def addtask():
     taskname=request.form['task']
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO todo_list (task) VALUES (%s)', (taskname,))
+    cursor.execute('INSERT INTO tasks (task_name,completed) VALUES (%s,%s)', (taskname,0))
     conn.commit()
     conn.close()
     return redirect('/')
@@ -36,21 +36,28 @@ def addtask():
 @app.route('/completion',methods=['POST'])
 def completion():
     id= request.form['id']
-    taskname = request.form['task']
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM todo_list WHERE id=(%s)', (id,))
-    cursor.execute('INSERT INTO complete_list (task) VALUES (%s)', (taskname,))
+    cursor.execute('UPDATE tasks SET completed = %s WHERE id = %s', (1, id,))
     conn.commit()
     conn.close()
     return redirect('/')
 
+@app.route('/back',methods=['POST'])
+def back():
+    id= request.form['id']
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('UPDATE tasks SET completed = %s WHERE id = %s', (0, id,))
+    conn.commit()
+    conn.close()
+    return redirect('/')
 @app.route('/delete',methods=['POST'])
 def delete():
     id= request.form['id']
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM complete_list WHERE id=(%s)', (id,))
+    cursor.execute('DELETE FROM tasks WHERE id=(%s)', (id,))
     conn.commit()
     conn.close()
     return redirect('/')
